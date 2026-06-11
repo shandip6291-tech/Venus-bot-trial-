8a9773da138a02205d64ab4048b6a9ab6e5d2c21
shandip6291-tech <shandip6291@gmail.com>
2026-06-11 14:37:19 +0530

    Initialize Discord bot with dotenv and intents

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Venus Music Bot Starting...");
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} online`);
+});
+
+client.login(process.env.TOKEN);
