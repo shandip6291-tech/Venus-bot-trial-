323ac78ac5d9849e330bf6d50f4720a73117726e
shandip6291-tech <shandip6291@gmail.com>
2026-06-11 15:57:23 +0530

    Fix Venus beat status

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
 client.once("ready", () => {
     console.log(`${client.user.tag} online`);
 
     client.user.setPresence({
         activities: [{
             name: process.env.STATUS || ".help | Venus Ultra",
-            type: 3
+            type: ActivityType.Listening
         }],
         status: "online"
     });
 });
+
+client.login(process.env.TOKEN);
